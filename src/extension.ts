eb4cde3bad8cc08f468874907d57a9927157d62c
mralex1810 <sash39av@gmail.com>
2023-06-21 11:00:40 +0300

    comment

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ const runJava = spawn('java', ['-jar',
 	dirName.join(path.sep) + path.sep + "src" + path.sep + 'persistent-caches-1.0-SNAPSHOT-shaded.jar',
 	workspaceFolder,
 	"true", // reset db
-	"parseHead", // parseHead
+	"parseHead", // parseHead or parseAll
 	"true", // enable trigram index
 	"true", // enable camel case index
 ]);
